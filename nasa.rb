5d2c909e5d477f76c86f1e93db885a06f415624c
Miguel Liendo <mliendo05@gmail.com>
2021-03-25 22:17:26 -0300

    segundo commit

--- nasa.rb
+++ nasa.rb
@@ -13,3 +13,5 @@ def request(link,key)
     response = https.request(request)
     return JSON.parse (response.read_body)
 end
+
+data = request("https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos?sol=1000&api_key=","r0IVneVCawcka91m55mRUuVGhMuSmgz3ibtoJhzv")
