a7e161d7718f21c4f76274b18c63d35d2a26fd27
Miguel Liendo <mliendo05@gmail.com>
2021-03-25 22:18:54 -0300

    tercer commit

--- nasa.rb
+++ nasa.rb
@@ -14,4 +14,21 @@ def request(link,key)
     return JSON.parse (response.read_body)
 end
 
+html = 
+    '<!DOCTYPE html>
+    <html lang="es">
+    <head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Photos api nasa</title>
+    </head>
+    <body>
+    <ul>'
+    
+data["photos"].each do |fotos|
+    fotos["img_src"]
+    html += "<li><img src='#{fotos['img_src']}'></li>\n" 
+end
+
 data = request("https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos?sol=1000&api_key=","r0IVneVCawcka91m55mRUuVGhMuSmgz3ibtoJhzv")
